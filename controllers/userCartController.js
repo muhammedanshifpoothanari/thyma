c4d493182f1507f51e026301d21b6450a0a88dda
muhammedanshifpoothanari <121426946+muhammedanshifpoothanari@users.noreply.github.com>
2023-05-05 12:12:42 +0530

    login ui change v51

--- controllers/userCartController.js
+++ controllers/userCartController.js
@@ -176,7 +176,7 @@ const addToCart = async (req, res) => {
   
       if (discount) {
 
-        if(combined[i].unitInStock>=quantity){ 
+        if(combined[1].unitInStock>=quantity){ 
           const discountedSubtotal = ((subtotal * (1 - (discount/100)))).toFixed(2);
           console.log(discountedSubtotal+'nd fxfdg'); 
           res.status(200).send({
